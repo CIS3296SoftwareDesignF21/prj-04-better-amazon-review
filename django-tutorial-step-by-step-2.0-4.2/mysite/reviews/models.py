edb4929e01fdeabde61e821133e189c17be3a6bc
Marshall Walsh <tum92783@temple.edu>
2021-11-09 18:42:13 -0500

    created reviews database model+table

--- django-tutorial-step-by-step-2.0-4.2/mysite/reviews/models.py
+++ django-tutorial-step-by-step-2.0-4.2/mysite/reviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    review_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=50)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    review_title = models.CharField(max_length=128)
+    review_content = models.CharField(max_length=3000)
+    review_url = models.CharField(max_length=255)
+    found_helpful = models.IntegerField(default=0)
+    verified_purchase = models.BooleanField(default=False)
+    product_name = models.CharField(max_length=255)
