8b65dd709790ecdb66e7f308282a485d2833b558
tuj52977 <tuj52977@temple.edu>
2021-11-17 12:33:51 -0500

    Moved scraper files around to reflect main branch and ease merging

--- main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("Hello, this is Max")
-    print("Hello from Marshall")
-    print("Hello, this is Juan")
-    print("Hello, this is Ben")
-if __name__ == "__main__":
-    main()
