4e6cd3fc1bb017c769ac7bddc6aea761622bf588
Marshall Walsh <tum92783@temple.edu>
2021-11-21 16:04:51 -0500

    added ISBN and review id to database table

--- django-tutorial-step-by-step-2.0-4.2/populate.py
+++ django-tutorial-step-by-step-2.0-4.2/populate.py
@@ -4,6 +4,6 @@ import sqlite3
 conn = sqlite3.connect('book_database.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE reviews (title text, content text, date text, variant text, images text, verified text, author text, rating int, product text, url text)''')
-reviews = pd.read_csv('bookData.csv')
+c.execute('''CREATE TABLE reviews (title text, content text, date text, variant text, images text, verified text, author text, rating int, product text, url text, ISBN text, reviewID text)''')
+reviews = pd.read_csv('bookData4.csv')
 reviews.to_sql('reviews', conn, if_exists='append', index=False)
