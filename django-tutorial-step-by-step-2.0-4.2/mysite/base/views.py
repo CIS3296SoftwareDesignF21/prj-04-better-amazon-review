1ccbbc025f8f087d290a008a548491f66797cf78
Benjamin Westburg <tun60968@temple.edu>
2021-11-22 12:49:13 -0500

    file changes.

--- django-tutorial-step-by-step-2.0-4.2/mysite/base/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def home(request):
-    return render(request,'home.html')
-
-def library(request):
-    return render(request, 'library.html')
